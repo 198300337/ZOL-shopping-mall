0e52fbc6565900c59ce6ac09f88316d19dd4c28f
198300337 <198300337@qq.com>
2021-01-04 08:44:18 +0800

    第一次提交

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
-$(function() {
-    $('')
-})
+window.onload = function() {
+    $(function() {
+            $('.nav .nav-right .number').hover(function() {
+                $('.number .number-bottom').css({ 'display': 'block', 'z-index': '5' })
+            }, function() {
+                $('.nav .number .number-bottom').css('display', 'none')
+            });
+        }),
+        $(function() {
+            var classify = $('.content .gather li');
+            // console.log(classify);
+            $.each(classify, function(i, obj) {
+                $(obj).hover(function() {
+                    $('.content .gather .abs').css({ 'display': 'bloak' })
+                })
+            });
+        })
+}
